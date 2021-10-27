866dc4f79cc8a57c79823a1b647d1dbff862e450
Ricardo Gonzalez <ricardo@Ricardos-MacBook-Air.local>
2021-10-27 15:10:28 -0400

    Completed skeleton of TodoList class. Time to add API functionability

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -13,11 +13,45 @@ class Todo {
     const keys = Object.keys(propValues);
     keys.forEach(key => this[key] = propValues[key]);
   }
+
+  copy() {
+    const copy = {};
+    const keys = Object.keys(this);
+    keys.forEach(key => copy[key] = this[key]);
+    return copy;
+  }
 }
 
 class TodoList {
+  #todos;
+
   constructor() {
-    this.todos = [];
+    this.#todos = [];
+  }
+
+  addTodo(todo) {
+    this.#todos.push(todo);
+  }
+
+  deleteTodo(id) {
+    const idx = this.#todos.findIndex(todo => todo.id === id);
+    this.#todos.splice(idx, 1);
+  }
+
+  updateTodo(id, propValues) {
+    this.getTodo(id).updateTodo(propValues);
+  }
+
+  getTodo(id) {
+    return this.#todos.find(todo => todo.id === id);
+  }
+
+  getAllTodos() {
+    return this.#todos.map(todo => todo.copy());
+  }
+
+  resetList() {
+    this.#todos = [];
   }
 }
 
@@ -39,5 +73,9 @@ class Controller {
 const app = new Controller(new Model(), new View());
 
 let todo = new Todo(1, "Finish project", "11", "11", "2021", false, "You need this for LS");
-todo.updateTodo({'#description': "You can do this!"});
+todo.updateTodo({description: "You can do this!"});
+
+let todoList = new TodoList();
+todoList.addTodo(todo);
+
 debugger;
