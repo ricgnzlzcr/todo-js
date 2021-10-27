15b1c1fb9bf3bf8d562865bacacc3b4daef35da6
Ricardo Gonzalez <ricardo@Ricardos-MacBook-Air.local>
2021-10-27 17:49:48 -0400

    API calls from Model complete

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,68 +1,18 @@
-class Todo {
-  constructor(id, title, day, month, year, completed, description) {
-    this.id = id;
-    this.title = title;
-    this.day = day;
-    this.month = month;
-    this.year = year;
-    this.completed = completed;
-    this.description = description;
-  }
 
-  updateTodo(propValues) {
-    const keys = Object.keys(propValues);
-    keys.forEach(key => this[key] = propValues[key]);
-  }
-
-  copy() {
-    const copy = {};
-    const keys = Object.keys(this);
-    keys.forEach(key => copy[key] = this[key]);
-    return copy;
-  }
-}
-
-class TodoList {
-  #todos;
-
-  constructor() {
-    this.#todos = [];
-  }
-
-  addTodo(todo) {
-    this.#todos.push(todo);
-  }
-
-  deleteTodo(id) {
-    const idx = this.#todos.findIndex(todo => todo.id === id);
-    this.#todos.splice(idx, 1);
-  }
-
-  updateTodo(id, propValues) {
-    this.getTodo(id).updateTodo(propValues);
-  }
-
-  getTodo(id) {
-    return this.#todos.find(todo => todo.id === id);
-  }
-
-  getAllTodos() {
-    return this.#todos.map(todo => todo.copy());
-  }
-
-  resetList() {
-    this.#todos = [];
-  }
-}
 
 class Model {
-  #todos;
 
   constructor() {
-    this.#todos = this.getServerTodos();
+    
   }
 
-  async getServerTodos() {
+  async getTodo(id) {
+    const response = await fetch(`/api/todos/${id}`);
+    const todo = await response.json();
+    return todo;
+  }
+
+  async getAllTodos() {
     const response = await fetch('/api/todos');
     const todos = await response.json();
     return todos;
@@ -84,15 +34,46 @@ class Model {
       }
     });
 
-    const todo = await response.json();
-    this.updateTodos();
-    return todo;
+    // if (response.status === 201) this.updateTodoList();
+    return response.status;
   }
 
-  async updateTodos() {
-    this.#todos = await this.getServerTodos();
-    console.log(this.#todos);
+  async deleteTodo(id) {
+    const response = await fetch(`/api/todos/${id}`, { method: 'DELETE' });
+    // if (response.status === 204) this.updateTodoList();
+    return response.status;
   }
+
+  async updateTodo(id, title, day, month, year, completed, description) {
+    const response = await fetch(`/api/todos/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+          title: title,
+          day: day,
+          month: month,
+          year: year,
+          completed: completed,
+          description: description
+      }),
+      headers: {
+          "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+
+    // if (response.status === 200) this.updateTodoList();
+    return response.status;
+  }
+
+  async resetTodos() {
+    const response = await fetch('/api/reset');
+    return response.status;
+  }
+
+  // async updateTodoList() {
+  //   this.#todos = await this.getServerTodos();
+  //   console.log(this.#todos);
+  // }
+
 
   
 
@@ -111,11 +92,12 @@ class Controller {
 
 const app = new Controller(new Model(), new View());
 
-let todo = new Todo(1, "Finish project", "11", "11", "2021", false, "You need this for LS");
-todo.updateTodo({description: "You can do this!"});
 
 let model = new Model();
-model.addTodo("Complete LS project", "01", "11", "2022", false, "Project for Core");
+let todo2 = (async () => {
+  let todo = await model.getTodo(9)
+  console.log(todo);
+})();
 
 
 // debugger;
